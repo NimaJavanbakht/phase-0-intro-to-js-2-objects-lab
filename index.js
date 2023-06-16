4b4c030290a878af2134ae2a98c65e016d740778
Nima Javanbakht <nimajavan230@gmail.com>
2023-06-16 16:28:24 -0700

    completes array lab 2.0

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: '11 BroadWay'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newKeyAndValue = {...employee}
+
+    newKeyAndValue[key] = value;
+    return newKeyAndValue;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newKeyAndValue = {...employee}
+
+    delete newKeyAndValue[key];
+    return newKeyAndValue;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
